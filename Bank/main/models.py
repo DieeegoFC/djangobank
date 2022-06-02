5e8042bcc6657accc7c4461d668b66b1b825801e
Diego Flores Chávez <diegofch.war@gmail.com>
2022-06-02 18:47:30 -0500

    DATABASE SETUP

--- Bank/main/models.py
+++ Bank/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Request(models.Model):
+    name = models.CharField(max_length=25)
+    first_surname = models.CharField(max_length=25)
+    second_surname = models.CharField(max_length=25)
+    cell_number = models.BigIntegerField()
+    birthday = models.CharField(max_length=10)
+    birthplace = models.CharField(max_length=25)
+    sex = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
+    card_type = models.CharField(max_length=25)
+    income = models.IntegerField()
+    curp = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.curp
