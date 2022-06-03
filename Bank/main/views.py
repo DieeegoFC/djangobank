f0f88cbc212fec243fc736b58e182e4c8e788511
Diego Flores Chávez <diegofch.war@gmail.com>
2022-06-02 19:14:05 -0500

    RETRIEVE DATABASE OBJECTS - index view

--- Bank/main/views.py
+++ Bank/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Request
 
 # Create your views here.
 
 def index(response):
-    return HttpResponse('<h1>Diego</h1>')
+    output = ''.join(x['curp'] for x in Request.objects.all().values())
+    return HttpResponse(output)
